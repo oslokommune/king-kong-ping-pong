b326a345f6857c9cec4fdb6871d7d58e596e91f7
Julius Pedersen <deifyed@ctemplar.com>
2020-06-26 16:26:45 +0200

    ✅ Add consecutive errors before notify

--- lib/ping.ts
+++ lib/ping.ts
@@ -1,9 +1,13 @@
 import axios from 'axios'
 import { IncomingWebhook } from '@slack/webhook'
 
-export function startPingJob (intervalMillis : number, upstreamURL : string, webhookURL: string, apiKey: string) {
+export function startPingJob (intervalMillis : number, upstreamURL : string, webhookURL: string, apiKey: string, max_duplicates : number) {
   const webhook : IncomingWebhook = new IncomingWebhook(webhookURL)
+  let knownStatus : string = ''
   let previousStatus : string = ''
+  let newStatus : string = ''
+  let msg : string = ''
+  let duplicateMessages : number = 0
 
   setInterval(async () => {
     try {
@@ -14,26 +18,32 @@ export function startPingJob (intervalMillis : number, upstreamURL : string, web
         headers: { apikey: apiKey }
       })
 
-      if (previousStatus === 'OK') return
-
-      previousStatus = 'OK'
-
-      webhook.send('I feel much better now')
+      newStatus = 'OK'
+      msg = 'I feel much better now'
     } catch (error) {
       let problem
 
       if (error.response) problem = error.response.status + ''
       else problem = 'no response'
 
-      if (previousStatus === problem) return
-
-      previousStatus = problem
-
-      webhook.send(
+      newStatus = problem
+      msg =
           `<!channel> I'm getting ${problem} when trying to ping myself..\n` +
           `Maybe someone else wants to try: \`curl -X "POST" -H "apikey: ${apiKey}" https://kkpp.api-test.oslo.kommune.no/pong\`\n` +
           'Wait! Take this: https://github.oslo.kommune.no/origodigi/kong/blob/master/README.md It will help you on your quest. God speed.'
-      )
     }
+
+    if (knownStatus === newStatus) return
+
+    if (previousStatus !== newStatus) {
+      previousStatus = newStatus
+      duplicateMessages = 0
+    }
+    else duplicateMessages += 1
+
+    if (duplicateMessages !== max_duplicates) return
+
+    knownStatus = newStatus
+    webhook.send(msg)
   }, intervalMillis)
 }
--- server.ts
+++ server.ts
@@ -8,9 +8,11 @@ import { startPingJob } from "./lib/ping";
 
 const PORT : string = process.env.PORT || '3000'
 const PING_INTERVAL_MS : string = process.env.PING_INTERVAL_MS || '5000'
+const INITIAL_PING_DELAY_MS : string = process.env.INITIAL_PING_DELAY_MS || '10000'
 const UPSTREAM_URL : string = process.env.UPSTREAM_URL || ''
 const SLACK_WEBHOOK_URL : string = process.env.SLACK_WEBHOOK_URL || ''
 const KONG_API_KEY : string = process.env.KONG_API_KEY || ''
+const TRIES_BEFORE_NOTIFY : string = process.env.TRIES_BEFORE_NOTIFY || '3'
 
 if (!UPSTREAM_URL) throw new Error('Missing upstream url to itself. Please configure the UPSTREAM_URL env variable')
 if (!SLACK_WEBHOOK_URL) throw new Error('Missing slack webhook URL. Please configure the SLACK_WEBHOOK_URL env variable')
@@ -20,12 +22,15 @@ function runServer(app: express.Application) {
 	const server : http.Server = app.listen(PORT, () => {
 		console.log(`Listening on ${PORT}`)
 
-		startPingJob(
-			Number.parseInt(PING_INTERVAL_MS),
-			UPSTREAM_URL,
-			SLACK_WEBHOOK_URL,
-			KONG_API_KEY
-		)
+		setTimeout(() => {
+			startPingJob(
+				Number.parseInt(PING_INTERVAL_MS),
+				UPSTREAM_URL,
+				SLACK_WEBHOOK_URL,
+				KONG_API_KEY,
+				Number.parseInt(TRIES_BEFORE_NOTIFY)
+			)
+		}, Number.parseInt(INITIAL_PING_DELAY_MS))
 	})
 
 	process.on('SIGINT', () => {
